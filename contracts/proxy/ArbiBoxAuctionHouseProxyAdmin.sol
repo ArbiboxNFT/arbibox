eb007fd276bb9d290fbfbaf5e056e097c1b27374
Nate <89227631+natedotv2@users.noreply.github.com>
2022-12-02 14:22:44 +0100

    Fixed naming

--- contracts/proxy/ArbiBoxAuctionHouseProxyAdmin.sol
+++ contracts/proxy/ArbiBoxAuctionHouseProxyAdmin.sol
@@ -4,4 +4,4 @@ pragma solidity ^0.8.6;
 
 import { ProxyAdmin } from '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol';
 
-contract SweepersAuctionHouseProxyAdmin is ProxyAdmin {}
+contract ArbiBoxAuctionHouseProxyAdmin is ProxyAdmin {}
