1046fafb33cdf862dd798a6457fda4dd9cadf180
Nate <89227631+natedotv2@users.noreply.github.com>
2022-12-05 03:59:49 +0100

    init new config

---
+++ .solcover.js
@@ -0,0 +1,12 @@
+module.exports = {
+    skipFiles: [
+      // WETH is for testing purposes only
+      'test/WETH.sol',
+    ],
+    configureYulOptimizer: true,
+    mocha: {
+      grep: "@skip-on-coverage", // Find everything with this tag
+      invert: true               // Run the grep's inverse set.
+    }
+  };
+  
